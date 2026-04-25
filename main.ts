704201d234bfbf4dc95993618866a1bd162f8b8a
Barry.C <wjyzxhm888@163.com>
2026-04-25 11:13:37 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -24,6 +24,21 @@ namespace mqlib {
      */
     //% subcategory="oled"
     //% group='oled-使用画布画图'
+    //% weight=100
+    //% block="oled点阵16x16_2"
+    //% imageLiteral=1
+    //% imageLiteralColumns=16
+    //% imageLiteralRows=16
+    //% imageLiteralScale=2
+    //% shim=images::createImage
+    export function oledCharacterPixels1616_2(i: string): Image {
+        return <Image><any>i;
+    }
+    /**
+     * Create a 16x16 pixel matrix for use as a custom character.
+     */
+    //% subcategory="oled"
+    //% group='oled-使用画布画图'
     //% block="oled画图16x16 $im 在位置x: $xStart, y $yStart"
     //% weight=99
     export function oledDrawImgWithPixels1616(im: Image, xStart: number = 0, yStart: number = 0): void {
